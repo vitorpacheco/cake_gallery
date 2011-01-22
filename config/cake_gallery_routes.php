b62c26bc6cd4280a7e5c280298e1ec04badb5331
Vitor Pacheco <vitor-p.c@hotmail.com>
2011-01-21 23:50:54 -0300

    some changes on routes file

--- config/cake_gallery_routes.php
+++ config/cake_gallery_routes.php
@@ -1,19 +1,38 @@
 <?php
-Router::connect('/cake_gallery', array(
-	'plugin' => 'cake_gallery',
-	'controller' => 'albums',
-	'action' => 'index'
-));
-Router::connect('/gallery/albums', array(
-	'plugin' => 'cake_gallery',
-	'controller' => 'albums',
-	'action' => 'index'
-));
-Router::connect('/cake_gallery/album/:slug', array(
-	'plugin' => 'cake_gallery',
-	'controller' => 'albums',
-	'action' => 'view'
-), array(
-	'pass' => array('slug')
-));
+$config['CakeGallery']['index'] = array(
+	'/cake_gallery',
+	array(
+		'plugin' => 'cake_gallery',
+		'controller' => 'albums',
+		'action' => 'index'
+	)
+);
+$config['CakeGallery']['albums_index'] = array(
+	'/cake_gallery/albums',
+	array(
+		'plugin' => 'cake_gallery',
+		'controller' => 'albums',
+		'action' => 'index'
+	)
+);
+$config['CakeGallery']['album_slug'] = array(
+	'/cake_gallery/album/:slug',
+	array(
+		'plugin' => 'cake_gallery',
+		'controller' => 'albums',
+		'action' => 'view'
+	),
+	array(
+		'pass' => array('slug')
+	)
+);
+$config['CakeGallery']['admin_index'] = array(
+	'/admin/cake_gallery',
+	array(
+		'plugin' => 'cake_gallery',
+		'admin' => true,
+		'controller' => 'albums',
+		'action' => 'index'
+	)
+);
 
