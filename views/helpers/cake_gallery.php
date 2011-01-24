68fe0f59395741201cb5c7802a3c8711bbd4d680
Vitor Pacheco <vitor-p.c@hotmail.com>
2011-01-24 19:02:26 -0300

    some changes in the helper

--- views/helpers/cake_gallery.php
+++ views/helpers/cake_gallery.php
@@ -11,14 +11,28 @@
  */
 class CakeGalleryHelper extends AppHelper {
 
-	public function afterRender() {
-		if (ClassRegistry::getObject('view')) {
-			echo $this->View->element('gallery_include_js', array('plugin' => 'cake_gallery'));
-		}
+	/**
+	 * 
+	 * Enter description here ...
+	 * @var View
+	 */
+	public $View = false;
+	
+	public function __construct() {
+		parent::__construct();
+		$this->View = ClassRegistry::getObject('view');
 	}
-
+	
+	public function beforeRender() {
+			echo $this->View->element('gallery_include_js', array('plugin' => 'cake_gallery'));
+	}
+	
+	public function loadGallery($slug) {
+		echo $this->View->element('gallery_album', array('plugin' => 'cake_gallery', 'slug' => $slug));
+	}
+	
 	public function replaceForAlbum($subject){
-		preg_match('/\[Gallery:(.*)\]/', $subject[0], $matches);
+		preg_match('/\[CakeGallery:(.*)\]/', $subject[0], $matches);
 		return $this->View->element('gallery_album', array('plugin' => 'cake_gallery', 'slug' => $matches[1], 'cache' => array('key' => $matches[1], 'time' => '5 mins')));
 	}
 }
